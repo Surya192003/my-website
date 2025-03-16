ab6bc0545f226f8e03bd71392cc2071bf50f5d5f
Surya192003 <suryasonofsubbu@gmail.com>
2025-03-16 00:54:23 +0000

    latest

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,11 @@
-// footer.component.ts
 import { Component } from '@angular/core';
 import { 
   faFacebookF, 
   faInstagram, 
   faWhatsapp 
 } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'; // Correct import path
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-footer',
@@ -13,8 +13,35 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons'; // Correct impor
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  // Define icons
   faFacebook = faFacebookF;
   faInstagram = faInstagram;
   faWhatsapp = faWhatsapp;
-  faEnvelope = faEnvelope; 
+  faEnvelope = faEnvelope;
+
+  // Sanitized icons for use in templates
+  facebookIcon: SafeHtml;
+  instagramIcon: SafeHtml;
+  whatsappIcon: SafeHtml;
+  envelopeIcon: SafeHtml;
+
+  constructor(private sanitizer: DomSanitizer) {
+    // Construct and sanitize SVG icons
+    this.facebookIcon = this.getSanitizedIcon(faFacebookF);
+    this.instagramIcon = this.getSanitizedIcon(faInstagram);
+    this.whatsappIcon = this.getSanitizedIcon(faWhatsapp);
+    this.envelopeIcon = this.getSanitizedIcon(faEnvelope);
+  }
+
+  // Helper function to construct and sanitize SVG icon
+  private getSanitizedIcon(icon: any): SafeHtml {
+    const svg = `
+      <svg aria-hidden="true" focusable="false" data-prefix="${icon.prefix}" 
+           data-icon="${icon.iconName}" class="svg-inline--fa fa-${icon.iconName}" 
+           role="img" xmlns="http://www.w3.org/2000/svg" 
+           viewBox="0 0 ${icon.icon[0]} ${icon.icon[1]}">
+        <path fill="currentColor" d="${icon.icon[4]}"></path>
+      </svg>`;
+    return this.sanitizer.bypassSecurityTrustHtml(svg);
+  }
 }
