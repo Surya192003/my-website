ebb8d50cb32745652bab3c098a299ddaea67579b
Surya192003 <suryasonofsubbu@gmail.com>
2025-03-18 15:31:58 +0000

    latest upadtes

--- src/app/classical-styles/classical-styles.component.ts
+++ src/app/classical-styles/classical-styles.component.ts
@@ -7,7 +7,29 @@ import { Location } from '@angular/common';
   styleUrls: ['./classical-styles.component.css']
 })
 export class ClassicalStylesComponent {
-  classicalStyles = ['Bharatanatyam', 'Kuchipudi'];
+  instructors = [
+    {
+      name: 'Chethanaa',
+      dob: new Date(2003, 3, 29), // Feb 21, 2003
+      styles: ['Kuchipudi'],
+      achievements: ['Certified in Kuchipudi'],
+      instagram: 'https://www.instagram.com/_chethanaa_0429?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==',
+      photo: '/assets/Chet.png'
+    }
+  ];
+
+  calculateAge(dob: Date): number {
+    const today = new Date();
+    const birthDate = new Date(dob);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+    
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  }
+
 
   constructor(private location: Location) {}
 
